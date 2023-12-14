976fcaed12fde57d23af445487c4358fcbfed729
Imre Bartis <imbartis@gmail.com>
2023-12-14 02:57:55 +0200

    add solution for exercise 1.3

--- osa1/tehtava1.2/src/Total.jsx
+++ osa1/tehtava1.2/src/Total.jsx
@@ -1,5 +1,5 @@
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+const Total = ({ part1, part2, part3 }) => {
+  const total = part1.exercises + part2.exercises + part3.exercises;
   return <p>Number of exercises {total}</p>;
 };
 
