7ff17c3a9f25e94301417d8dcf5029ba50a7fe57
Imre Bartis <imbartis@gmail.com>
2023-12-17 05:27:14 +0200

    add solution for exercise 1.7

--- osa1/tehtava1.6/src/Statistics.jsx
+++ osa1/tehtava1.6/src/Statistics.jsx
@@ -1,10 +1,27 @@
-const Statistics = ({ good, neutral, bad }) => (
-  <div>
-    <h2>statistics</h2>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </div>
-);
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  );
+};
 
 export default Statistics;
