def8bb22873d7f7c1c02238bd777e61404b35471
Imre Bartis <imbartis@gmail.com>
2024-05-12 03:10:11 +0300

    tehtävä 4.9.

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,18 +2,37 @@ const blogsRouter = require('express').Router()
 
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
     response.json(blogs)
-  })
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
--- osa4/test/blog_api_test.js
+++ osa4/test/blog_api_test.js
@@ -28,6 +28,20 @@ test('there are two blogs', async () => {
   assert.strictEqual(response.body.length, 2)
 })
 
+test.only('MongoDB assigns an id property when a blog is added', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://www.testblog.com',
+    likes: 0
+  }
+
+  const returnedObject = await api.post('/api/blogs').send(newBlog)
+
+  assert.ok(returnedObject.body.hasOwnProperty('id'), 'The object does not have an id property')
+  assert.strictEqual(typeof returnedObject.body.id, 'string', 'the id is not a string')
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
