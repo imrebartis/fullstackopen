dabd2d980eb21e14ef22b0c1d69927b3f103283f
Imre Bartis <imbartis@gmail.com>
2024-06-06 15:34:26 +0300

    tehtävä 5.10.

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -183,9 +183,11 @@ const App = () => {
       <Togglable buttonLabel="new blog" ref={blogFormRef}>
         <BlogForm createBlog={addBlog} setErrorMessage={setErrorMessage} />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+        ))}
     </div>
   );
 };
