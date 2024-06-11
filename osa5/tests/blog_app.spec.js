f68398e64e5ce559be5250dd25004803a96dd186
Imre Bartis <imbartis@gmail.com>
2024-06-12 01:58:47 +0300

    tehtävä 5.17.

---
+++ osa5/tests/blog_app.spec.js
@@ -0,0 +1,24 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'testuser',
+        password: 'salainen',
+      },
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    const usernameInput = await page.$('input[name="Username"]')
+    const passwordInput = await page.$('input[name="Password"]')
+    expect(usernameInput).not.toBeNull()
+    expect(passwordInput).not.toBeNull()
+  })
+})
