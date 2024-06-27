31d8403aa9ba6fe2e080c80ddd49800baeca7c2a
Imre Bartis <imbartis@gmail.com>
2024-06-28 01:43:56 +0300

    tehtävä 6.9.

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { vote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === "ALL" || filter.trim().length < 3) {
+      return anecdotes;
+    } else {
+      return anecdotes.filter((anecdote) =>
+        anecdote.content.toLowerCase().includes(filter.trim().toLowerCase())
+      );
+    }
+  });
   const dispatch = useDispatch();
   const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
 
