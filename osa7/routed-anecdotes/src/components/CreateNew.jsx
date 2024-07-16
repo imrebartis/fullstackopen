b85b0dc774e85bc99d6883e848169dab395ce2ff
Imre Bartis <imbartis@gmail.com>
2024-07-16 14:59:14 +0300

    tehtävät 7.5. ja 7.6.

--- osa7/routed-anecdotes/src/components/CreateNew.jsx
+++ osa7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,24 +1,30 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useField } from "../hooks";
 
 const CreateNew = ({ addNew }) => {
-  const content = useField("name");
-  const author = useField("author");
-  const info = useField("info");
+  const { reset: resetContent, ...contentProps } = useField("name");
+  const { reset: resetAuthor, ...authorProps } = useField("author");
+  const { reset: resetInfo, ...infoProps } = useField("info");
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: contentProps.value,
+      author: authorProps.value,
+      info: infoProps.value,
       votes: 0,
     });
     navigate("/");
   };
+
+  const handleReset = (e) => {
+    e.preventDefault();
+    resetContent();
+    resetAuthor();
+    resetInfo();
+  }
 
   return (
     <div>
@@ -26,17 +32,18 @@ const CreateNew = ({ addNew }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...contentProps} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...authorProps} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...infoProps} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   );
