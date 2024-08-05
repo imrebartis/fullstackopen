e2d9069fc817ca022ffbcbfe88637af31f85cbea
Imre Bartis <imbartis@gmail.com>
2024-08-06 02:20:54 +0300

    tehtävä 8.7.

--- osa8/books-graphql-backend/index.js
+++ osa8/books-graphql-backend/index.js
@@ -125,6 +125,11 @@ type Author {
       author: String!
       genres: [String!]!
     ): Book
+
+     editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -175,6 +180,16 @@ const resolvers = {
       const newBook = { ...args, id: uuid() }
       books = books.concat(newBook)
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
     }
   }
 }
