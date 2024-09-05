3a08ac11f07e16b803a2c010d581deccdb62f4a3
Imre Bartis <imbartis@gmail.com>
2024-09-05 23:42:03 +0300

    tehtävä 9.10.

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 app.use(express.json());
 
@@ -12,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
